80bded91d0139bad55405f3c781f92345b44f52f
adityachaturvedi13 <119115616+adityachaturvedi13@users.noreply.github.com>
2022-11-26 18:56:06 +0530

    Add files via upload

---
+++ project19.py
@@ -0,0 +1,17 @@
+n=int(input("Emter the number: "))
+flag=0
+for i in range(2,n):
+    if n%i==0:
+        flag=1
+        break
+    else:
+        x=n
+        sum=0
+        while x>0:
+            r=x%10
+            sum=sum*10+r
+            x=x//10
+if sum==n:
+    print(n,"is a prime pallindrome")
+else:
+    print(n,"is not a prime pallindrome")   
